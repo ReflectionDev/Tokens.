fd1b2398550428062694383803e862ff8d303b7c
RohitDev <80133076+ReflectionDev@users.noreply.github.com>
2023-06-01 03:16:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 const token = [
-    "ODgxMjc4NTY2MzcxODI3NzEz.GTFRje._DYiVagzRUl_ZPM1cpDdIf2gByzj0otcBrupQM",//belil
-    "ODI2NDA4MDM5ODM3NzI4NzY4.GqtF3Q.QZ5BwIrkcAkp43lJHnUnStynDz3PV4TYaouVWQ",//ilyas
-    "ODg0MjM2ODI4OTY4ODg2Mjg0.GgXAIj.mwnr4opeaBXduiwaDIFujDqMG2kqfWO_jfZfrk",//khelilayoub797
-    "MTEwMjMwMDc4MzkzODExNzcwMg.GO9PqQ.Cpcy0C7e-hyu6INVzxU3iLIpCgf8kel05S_RWY",//khelil1
-    "ODg0MjM3NzkyODcwMjg5NDI4.GiRf7o.NXtCn71vj-uOkGZaDeMt36nsrb4i8jQEycVTb8",//khelulayoub
-    "ODUxMzM0MjQxOTM2ODY3MzU4.GaJT6R.snEPWpRp94CF8jsynV3ny1N_2eG1H4LvZd08vw",//krym
-    "ODQ0MDc5NzExMTkzMDA2MTAx.Gs5eAz.MQsGCsks7c2BaF4OpwNJWb0mKojaWuNCh9u4QY",//hehe
-    "ODQzMDgwNjM1MzI0NDMyMzk0.GjXp-v.7A8CrRtP38Iu10qHC3ZD7vAdbo72YEram4Zh34",//respect
-    "ODA2Mjc1MTY2NzU2OTI5NTY2.GHt-lc._D_R25PdL8UH6q4kGDMdAAZgMd4sDG6pFIoK_8",//unflectiiual
-    "ODQ2MzgzMDU1OTYxOTgwOTQ4.GUkBPF.1Vh57NQ28H8v2yOlyo8UsnVU9cm_t_SLTUJdas",//amr
-    "NzcxMzUxODg2NzA3NDI1MzEw.Gk-DD5.oozE93m7SeTDnsFkGTdDVna9j0cWEpiowhN-sU",//xhaj
-    "ODE3OTM4MzU1ODUyNjczMDg2.GUTslQ.DP6-W4Bi7Nnh38MOAyRz4TsxEQQwYKt_K6NR-o",//oliver
-    "ODUzMzExOTE1MzcwMjE3NDgz.GZVjdV.ehHxUCyZXknM7Y3tqFq3NT1K_aIqKd9RajtRjI",//unknown
-    "ODY1NzI5MzM2NTM5NjExMTQ4.GONPq3.hbWLQowaENzk1UqrwJD_FOnrYN9_86FSsRfx3E",//rtfriska
-    "ODQzMTc0MjAyMzIwMzU1MzM4.GSwYsu.TALQeZo6fkrh6mrFXX3GwMmu_aTGi8gAXrMq_8",//asssse7 
-    "Nzc4NjM5Njk4ODUxMDA0NDY2.GIpB2f.PXzxeMDVb0m3dfhWZGOlMK_uw75KRsdWRCH03E",//ayobkhelil99
-    "MTEwMTMwNjIxMDYyNTU0MDE4OQ.GEzaeV.peCoZFeRbbgFSuBUF8uTdvlEbqoGOYjCNjU5wU",//ayoub.video.17
-    "ODUzMDk1MjIyNTMxNTg4MTI2.Gu_OhW.xPrfx0k6cL3RoRJorVaDZGAzpqbVr_-MZGGMTg",//atct
-    "MTA2MTExOTY0MDAxODg4MjU2MA.GOYkXp.0LX7oRIHqgT18SSKFAneV5HqSMtEFkGYSuEXqg",//reflectionpain
-    "MTA1MTQyODU1NzkzMjQ3MDMxNA.Gir1tK.vjYq5kNqAkxG8l15F0tzigu4IbMC1YKgwDbBEI",//rakan111229
-    "MTA2MTU4MTk4NDEyNTcwMjE5NA.GZL_dg.3YIR_jprlN2u0ztrIaYAFUl5CcaUgpxDZFnrLo",//jory
-    "ODQzMDE3OTU3OTc3ODgyNjI2.Gxoxpk.X1aGE4_VK7ieZmXCy-RROZTpvlsiOs9-a72AxY",//noah
-    "ODMyMzU2NzcxMTY2Mjg5OTkw.GP03_2.TkM1rGHChH0JrMSPe1ibcrwuBPe2y_4UI9OWkE",//enzo
-    "ODg0MjM5ODk3NjMwNzM2NDU1.G39uZp.gOHUTkM3VdrvQWqje-nkd60xYNwcGRkH0Cbpog",//palmari
-    "ODg1NTA2MjgwODg4ODY4OTI3.GP_-ca.hFQgghIvzlA-yjtQuxXDIgwLh7lQlUPyAQ0CY0",//owo
-    "NjcwNjY5NDY4ODc1MDk2MDY3.Gzm_R_.iMeR64DxwGVv3c8gf5pBesU_UroDhzccf8V9y0",//khelila6
-    "MTA5NDY4NTQzNDg4NzU0NDgzNQ.GwL0rn.wO4dGlUyAodweY966GawgQEehbAVO83x9KY6Ws",//ayoub1712200
-    "MTA3NTM5ODQ5MzY4Mzg1MTI5NA.Gvqnm5.xOPIjlv2saL0qTILfv95OHdTxX7hYngjIqZllQ",//ayoub80
-    "MTA5OTkyMzkzMTc1NTk3NDY1OA.Ghp7RE.UAGzu5juKUuSF51MWd9IDM71w6Ua3i08BFpxuU",//sbydrayzr
-    "MTA4MDg4NDU5NjA0NjAzNzAxMw.GalEqO.75tMKCVHwRrwT2jh3F4_4sRMXqNCDIuGY6QO50",//hessahayoub
-    "MTA3Mzc5MTE0OTEzNjk0MTA1Ng.GXbr0_.gNLFYvCBMDfrPvLzbdjxOn7ux8fc__Ax-imZGc",//zfamous191
-    "MTA4MjcyMDU4MTg4OTI5ODQ2Mw.Gc-_kX.2ZvL6FsVUN-GZI9wm-wc1T_tZEb71saGfHRgp4",//deadfjikk
-    "MTA5MDUxMzc3NzY4Mzg1NzUzOQ.Gw_mSC.B8VlNAUH5EDN4tRZHAR_KSxeHwE3P7txzOZ3eg",//ederson
+
     ];
     token.forEach(token => {
     const Discord = require("discord.js-selfbot");
